1568bc57cbef2a91926e9f484fe008755332d4c0
ssupix <151896598+ssupix@users.noreply.github.com>
2024-07-11 22:21:51 -0700

    done

--- AppDevStrat MVC/site/js/script.js
+++ AppDevStrat MVC/site/js/script.js
@@ -13,30 +13,71 @@
 // push() adds new items to an array
 // === NEW: ===
 // callback : Wait till a prior function is complete before "calling it"
-// _commit a method we are making to write changes to our list (todos)
+// _commit a method we are making to write changes to our list (todo)
 
 
 
 
 class Model {
-  constructor() {}
 
+  constructor() {
+    // gets a JSON string from local storage and converts to JS object
+    this.todos = JSON.parse(localStorage.getItem('todos')) || []
+  }
 
+  bindTodoListChanged(callback) {
+    // waits for the list to be changed
+    this.onTodoListChanged = callback
+  }
+
+  // 
+  _commit(todos) {
+    // two function tied into one waiting to be called
+    this.onTodoListChanged(todos)
+    localStorage.setItem('todo', JSON.stringify(todos))
+  }
+
+  // Start of Adding, Editing, Deleting and toggling
+  // at the end you an even trial this in the console:
+  // app.model.addTodo('Go to Gym), to see how it works (ie visual layer isn't required.)
+
+  addTodo(todoText) {
+    const todo = {
+      id       : this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text     : todoText,
+      complete : false,
+    }
+
+    this.todos.push(todo)
+    this._commit(this.todos)
+  }
+
+  editTodo(id, updateText) {
+    this.todos = this.todos.map( todo =>
+      todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete} : todo
+    )
+
+    this._commit(this.todo)
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter(todo => todo.id !== id)
+    this._commit(this.todos)
+  }
+
+  toggleTodo(id) {
+    this.todos = this.todos.map(todo =>
+        todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
+    )
+
+    this._commit(this.todos)
+  }
 }
 
-
-
-
-
-
-
-
-
-
 /*
- @class View
+@class View
 
- Visual representation of the model.
+Visual representation of the model.
  */
 
 // View: Through manipulation of the DOM we will generate dynamic objects on the page. Typically this is where JS frameworks such as Angular, ReactJS, or Vue doing most of the work. 
@@ -68,21 +109,40 @@ Common structure/template
 // <form> <input type="text" name="todo" placeholder="Add Event Here"> <button>
 
 class View {
-  constructor() {}
+  constructor() {
+    this.app = this.getElement('#container')
+    this.form = this.createElement('form')
+    this.input = this.createElement('input')
+    this.input.type = 'text'
+    this.input.placeholder = 'Add Event Here' 
+    this.input.name = 'todo'
+    this.submitButton = this.createElement('button')
+    this.submitButton.textContent = '+'
+    this.form.append(this.input, this.submitButton)
+    // this.tittle = this.createElement('h1)
+    // this.title.textContent = 'Todos'
+    this.todoList = this.createElement('ul', 'todo-list')
+    this.app.append(this.form, this.todoList)
 
+    this._temporaryTodoText = ''
+    this._initLocalListeners()
+  }
 
+  get _todoText() {
+    return this.input.value
+  }
 
+  _resetInput() {
+    this.input.value = ''
+  }
 
+  createElement(tag, className) {
+    const element = document.createElement(tag)
 
+    if (className) element.classList.add(className)
 
-
-
-
-
-
-
-
-
+      return element
+  }
 
 // ++++++++++++++++++++++++++++++++++++  View code complete after this point
   getElement(selector) {
@@ -201,7 +261,7 @@ Links the user input and the view output.
 
 /* ===========   
 Lastly the controller is the link between the model (the data ) and the view (what the user will see on the page)
- The Controller will handle events after they're fired. When you submit a new todo, or click the delete button, or click on the checkbox of a todo, an event will be fired. The view section (above) is currently using bind to "listen" for our main commands of Add Delete Edit and Toggle.
+The Controller will handle events after they're fired. When you submit a new todo, or click the delete button, or click on the checkbox of a todo, an event will be fired. The view section (above) is currently using bind to "listen" for our main commands of Add Delete Edit and Toggle.
 ============ */ 
 
 
@@ -210,9 +270,39 @@ class Controller {
   constructor(model, view) {
     this.model = model
     this.view = view
-  }
-}
 
+    //Explicit the binding
+    this.model.bindTodoListChanged(this.onTodoListChanged)
+    this.view.bindAddTodo(this.handleAddTodo)
+    this.view.bindDeleteTodo(this.handleDeleteTodo)
+    this.view.bindEditTodo(this.handleEditTodo)
+    this.view.bindToggleTodo(this.handleToggleTodo)
+
+    // Display the initial todos when onTodoListChanged is called (after every change)
+    this.onTodoListChanged(this.model.todos)
+  }
+
+  onTodoListChanged = todo => {
+    this.view.displayTodos(todo)
+  }
+
+  handleAddTodo = todoText => {
+    this.model.addTodo(todoText)
+  }
+
+  handleEditTodo = (id, todoText) => {
+    this.model.editTodo(id, todoText)
+  }
+
+  handleDeleteTodo = (id) => {
+    this.model.deleteTodo(id)
+  }
+
+  handleToggleTodo = (id) => {
+    this.model.toggleTodo(id)
+  }
+
+}
 
 // Lastly, this generates a new object, based on the MVC structure
 
